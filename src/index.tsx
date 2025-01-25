d76cfd0e57d89fe4122f7295bb25ef8927754101
Stanislav Zatolokin <szatolokin.web@gmail.com>
2025-01-25 03:22:29 +0300

    wow

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,66 @@
 import "./index.css";
 
+// hello
+
 import ReactDOM from "react-dom/client";
 import { observer } from "mobx-react";
 import { authModel } from "./models/auth";
 
 const User: React.FC = observer(() => {
-    if (authModel.isLoading) {
-        return <div>Loading...</div>;
-    }
+  if (authModel.isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (authModel.errorMessage) {
-        return <div>{authModel.errorMessage}</div>;
-    }
+  if (authModel.errorMessage) {
+    return <div>{authModel.errorMessage}</div>;
+  }
 
-    const user = authModel.user;
+  const user = authModel.user;
 
-    if (user === undefined) {
-        return <div>Initial state</div>;
-    }
+  if (user === undefined) {
+    return <div>Initial state</div>;
+  }
 
-    return (
-        <div>
-            {user.id} {user.name}
-        </div>
-    );
+  return (
+    <div>
+      {user.id} {user.name}
+    </div>
+  );
 });
 
 const App: React.FC = () => {
-    return (
-        <>
-            <div>
-                <button
-                    onClick={() =>
-                        authModel.signIn({
-                            name: "signIn",
-                            password: "signIn",
-                        })
-                    }
-                >
-                    sign in
-                </button>
-                <button
-                    onClick={() =>
-                        authModel.signUp({
-                            name: "signUp",
-                            password: "signUp",
-                        })
-                    }
-                >
-                    sign up
-                </button>
-                <button onClick={() => authModel.signOut()}>sign out</button>
-            </div>
-            <User />
-        </>
-    );
+  return (
+    <>
+      <div>
+        <button
+          onClick={() =>
+            authModel.signIn({
+              name: "signIn",
+              password: "signIn",
+            })
+          }
+        >
+          sign in
+        </button>
+        <button
+          onClick={() =>
+            authModel.signUp({
+              name: "signUp",
+              password: "signUp",
+            })
+          }
+        >
+          sign up
+        </button>
+        <button onClick={() => authModel.signOut()}>sign out</button>
+      </div>
+      <User />
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(<App />);
